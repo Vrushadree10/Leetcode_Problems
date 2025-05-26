3ef117ebdc29247c79b39b40577a8bca33d32a29
KARAPAKULA BALAJI SAI VRUSHADREE <kbsv2004@gmail.com>
2025-05-26 20:14:59 +0530

    Add LeetCode solutions for 2025-05-23

---
+++ 2025-05-23/find-numbers-with-even-digits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        count = 0
+        for num in nums:
+            digits = 0
+            while num != 0:
+                digits += 1
+                num //= 10
+            if digits % 2 == 0:
+                count += 1
+        return count
