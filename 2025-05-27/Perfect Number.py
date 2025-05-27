d6a2fb8f5da24f7a137f38051319a19971a53594
KARAPAKULA BALAJI SAI VRUSHADREE <kbsv2004@gmail.com>
2025-05-27 19:26:34 +0530

    Add LeetCode solutions for 2025-05-27

---
+++ 2025-05-27/Perfect Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        if num <= 1:
+            return False
+        
+        total = 1  # 1 is always a divisor
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                total += i
+                if i != num // i:
+                    total += num // i
+            i += 1
+        return total == num
